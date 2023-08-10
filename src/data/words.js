4613dc39569867e1abab6aa196aac8496318e99b
luhoro <luisashorowitz@gmail.com>
2023-08-09 23:08:48 -0300

    feat more words in data

--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,12 @@
 export const wordsList = {
-  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Parachoque", "Volante", "Marcha", "Parabrisa"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Goiaba", "Abacaxi", "Maracujá", "Melância"],
-  corpo: ["Braço", "Perna", "Dedos", "Pescoço", "Olhos", "Cabeça", "Costas", "Joelho", "Cotovelo", "Tornozelo"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React", "Bootstrap", "Typescript", "Node", ""],
-  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão", "Cenoura", "Alface"],
-};
+  Carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Para-choque", "Volante", "Marcha", "Para-brisa", "Porta-luvas", "Porta-malas"],
+  Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Goiaba", "Abacaxi", "Maracujá", "Melância", "Abacate", "Jabuticaba", "Açaí", "Framboesa", "Limão"],
+  Corpo: ["Braço", "Perna", "Dedos", "Pescoço", "Olhos", "Cabeça", "Costas", "Joelho", "Cotovelo", "Tornozelo", "Nariz", "Orelha", "Ombro", "Barriga", "Bunda", "Coxa", "Panturrilha"],
+  Computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Carregador", "Entrada USB", "Mousepad"],
+  Programação: ["Linguagem", "Framework", "JavaScript", "React-js", "Bootstrap", "Typescript", "Node-js", "Biblioteca", "Java", "Python"],
+  Alimento: [ "Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão", "Alho", "Azeitona"],
+  Vegetal: ["Cenoura", "Alface", "Cebola", "Abobrinha", "Coentro", "Pepino", "Beterraba"],
+  Sobremesa: ["Sorvete", "bolo", "picolé", "Milkshake", "Pudim", "Beijinho", "Brigadeiro", "Balinha", "Pirulito"],
+  Esporte: [ "Basquete", "Vôlei", "Futebol", "Handball", "Natação", "Ciclismo", "Corrida", "Tênis", "Tênis de mesa", "Futsal",],
+  Vestuário: ["Calça", "Blusa", "Camisa", "Camiseta", "Tênis", "Meia", "Óculos", "Pulseira", "Colar", "Vestido", "Saia", "Meia-calça", "Bota", "Luva" ]
+}
